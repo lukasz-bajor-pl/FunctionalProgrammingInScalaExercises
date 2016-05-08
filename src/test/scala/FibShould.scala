559bf5f1a7575aa0df16a505776ca1c9a37bffb7
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 11:59:10 +0200

    Exercise 2.1 - case for 0th element - failing test

---
+++ src/test/scala/FibShould.scala
@@ -0,0 +1,13 @@
+import chapter2.exercise1.Fib
+import org.scalatest.FreeSpec
+
+/**
+  * Created by lbajor on 2016-05-08.
+  */
+class FibShould extends FreeSpec {
+  "Fib.fib should" - {
+    "return 0 as first fibonacci number" in {
+      assert( 0 === Fib.fib(0) )
+    }
+  }
+}

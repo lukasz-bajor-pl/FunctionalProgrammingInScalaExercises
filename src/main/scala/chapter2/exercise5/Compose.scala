846bc71ba7583b0e5ffea91034054e798e0cd4b2
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 20:24:21 +0200

    Exercise 2.5 - setup + passing test

--- src/main/scala/chapter2/exercise5/Compose.scala
+++ src/main/scala/chapter2/exercise5/Compose.scala
@@ -4,5 +4,5 @@ package chapter2.exercise5
   * Created by lbajor on 2016-05-08.
   */
 object Compose {
-  def compose[A,B,C](f: B => C, g: A => B): A => C = ???
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
 }

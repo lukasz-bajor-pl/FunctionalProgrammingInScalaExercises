f8cf8d04a9057b5e8c76b40a2ebbc17dd8f63209
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 12:33:23 +0200

    Exercise 2.1 - case for 45th element - passing test with tail recursion + evern quicker with removed multiple params list

--- src/main/scala/chapter2/exercise1/Fib.scala
+++ src/main/scala/chapter2/exercise1/Fib.scala
@@ -8,14 +8,14 @@ object Fib {
     type FibValue = (Int, Int)
 
     @annotation.tailrec
-    def tailRecFib(prePrevFib: FibValue, prevFib: FibValue)(n: Int): Int = {
+    def tailRecFib(prePrevFib: FibValue, prevFib: FibValue, n: Int): Int = {
       n match {
         case prePrevFib._1 => prePrevFib._2
         case prevFib._1 => prevFib._2
-        case _ => tailRecFib(prevFib, (prevFib._1 + 1, prevFib._2 + prePrevFib._2))(n)
+        case _ => tailRecFib(prevFib, (prevFib._1 + 1, prevFib._2 + prePrevFib._2), n)
       }
     }
 
-    tailRecFib ((0, 0), (1, 1)) (n)
+    tailRecFib ((0, 0), (1, 1), n)
   }
 }

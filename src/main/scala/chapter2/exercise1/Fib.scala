353a9fd97f6b3db4cfef0148fdb952a3c0c2962d
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 12:02:14 +0200

    Exercise 2.1 - case for 1th element - passing test

--- src/main/scala/chapter2/exercise1/Fib.scala
+++ src/main/scala/chapter2/exercise1/Fib.scala
@@ -4,5 +4,9 @@ package chapter2.exercise1
   * Created by lbajor on 2016-05-08.
   */
 object Fib {
-  def fib(n: Int): Int = 0
+  def fib(n: Int): Int =
+    n match {
+      case 0 => 0
+      case _ => 1
+    }
 }

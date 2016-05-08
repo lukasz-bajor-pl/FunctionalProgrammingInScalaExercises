962ee88fbd383de023105c0aecef8ceea83ac5b2
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 13:34:24 +0200

    Exercise 2.2 - passing test for 2 el array and false returning function

--- src/main/scala/chapter2/exercise2/IsSorted.scala
+++ src/main/scala/chapter2/exercise2/IsSorted.scala
@@ -4,5 +4,10 @@ package chapter2.exercise2
   * Created by lbajor on 2016-05-08.
   */
 object IsSorted {
-  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = true
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean =
+  as.length match {
+    case 0 => true
+    case 1 => true
+    case _ => ordered(as.head, as.tail.head) && isSorted(as.tail.tail, ordered)
+  }
 }

7f5ef34812ac3c7ce109fd63704288642bdc974c
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 12:31:32 +0200

    Exercise 2.1 - case for 45th element - passing test with tail recursion

--- src/main/scala/chapter2/exercise1/Fib.scala
+++ src/main/scala/chapter2/exercise1/Fib.scala
@@ -4,10 +4,24 @@ package chapter2.exercise1
   * Created by lbajor on 2016-05-08.
   */
 object Fib {
-  def fib(n: Int): Int =
+  def fib(n: Int): Int = {
+    type FibValue = (Int, Int)
+
+    @annotation.tailrec
+    def tailRecFib(prePrevFib: FibValue, prevFib: FibValue)(n: Int): Int = {
+      n match {
+        case prePrevFib._1 => prePrevFib._2
+        case prevFib._1 => prevFib._2
+        case _ => tailRecFib(prevFib, (prevFib._1 + 1, prevFib._2 + prePrevFib._2))(n)
+      }
+    }
+
+    def initialTailRecFib(n: Int) = tailRecFib ((0, 0), (1, 1)) (n)
+
     n match {
       case 0 => 0
       case 1 => 1
-      case _ => fib(n-1) + fib(n-2)
+      case _ => initialTailRecFib(n - 1) + initialTailRecFib(n - 2)
     }
+  }
 }

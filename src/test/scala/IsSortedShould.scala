e4089082eed5112cd8339670a6c155d0ac66a3e3
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 13:27:22 +0200

    Exercise 2.2 - failing test for empty array

--- src/test/scala/IsSortedShould.scala
+++ src/test/scala/IsSortedShould.scala
@@ -1,4 +1,5 @@
 import chapter2.exercise1.Fib
+import chapter2.exercise2.IsSorted
 import org.scalatest.FreeSpec
 import org.scalatest.concurrent.TimeLimitedTests
 import org.scalatest.time.{Millis, Seconds, Span}
@@ -6,9 +7,13 @@ import org.scalatest.time.{Millis, Seconds, Span}
 /**
   * Created by lbajor on 2016-05-08.
   */
-class IsSorted extends FreeSpec with TimeLimitedTests {
+class IsSortedShould extends FreeSpec with TimeLimitedTests {
   val timeLimit = Span(100, Millis)
 
   "IsSorted.isSorted should" - {
+
+    "return true for empty array" in {
+      assert(true === IsSorted.isSorted(Array[Int](), (a: Int, b: Int) => false))
+    }
   }
 }

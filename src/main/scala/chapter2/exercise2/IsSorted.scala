3f74cba4e2211b8946bff78376091468cdaa3e9b
Lukasz Bajor <lukaszic@o2.pl>
2016-05-08 13:27:42 +0200

    Exercise 2.2 - passing test for empty array

--- src/main/scala/chapter2/exercise2/IsSorted.scala
+++ src/main/scala/chapter2/exercise2/IsSorted.scala
@@ -4,5 +4,5 @@ package chapter2.exercise2
   * Created by lbajor on 2016-05-08.
   */
 object IsSorted {
-  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = true
 }
